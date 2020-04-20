491c21fa7e6f6798ac562c06beb8d7ad6f0f542c
batro83 <batro_ska@yahoo.es>
2020-04-20 21:05:39 +0200

    init

---
+++ src/test/java/com/robot/app/test/RobotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robot.app.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
